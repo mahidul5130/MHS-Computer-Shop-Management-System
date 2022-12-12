81745174293f1fd8153e4b797f4a0b097d4ad751
Md. Mahidul Haque Sijan <mahidul5130@gmail.com>
2022-12-12 23:29:29 +0600

    main

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -1,47 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
-    <title>@yield('page_title')</title>
-    <link href="{{asset('front_assets/css/font-awesome.css')}}" rel="stylesheet">
-    <link href="{{asset('front_assets/css/bootstrap.css')}}" rel="stylesheet">   
-    <link href="{{asset('front_assets/css/jquery.smartmenus.bootstrap.css')}}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{asset('front_assets/css/jquery.simpleLens.css')}}">    
-    <link rel="stylesheet" type="text/css" href="{{asset('front_assets/css/slick.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('front_assets/css/nouislider.css')}}">
-    <link id="switcher" href="{{asset('front_assets/css/theme-color/default-theme.css')}}" rel="stylesheet">
-    <link href="{{asset('front_assets/css/sequence-theme.modern-slide-in.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('front_assets/css/style.css')}}" rel="stylesheet">    
 
-    <!-- Google Font -->
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-    
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('page_title')</title>
+  <link href="{{asset('front_assets/css/font-awesome.css')}}" rel="stylesheet">
+  <link href="{{asset('front_assets/css/bootstrap.css')}}" rel="stylesheet">
+  <link href="{{asset('front_assets/css/jquery.smartmenus.bootstrap.css')}}" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="{{asset('front_assets/css/jquery.simpleLens.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('front_assets/css/slick.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('front_assets/css/nouislider.css')}}">
+  <link id="switcher" href="{{asset('front_assets/css/theme-color/default-theme.css')}}" rel="stylesheet">
+  <link href="{{asset('front_assets/css/sequence-theme.modern-slide-in.css')}}" rel="stylesheet" media="all">
+  <link href="{{asset('front_assets/css/style.css')}}" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+  <!-- Google Font -->
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-      
-    <script>
-    var PRODUCT_IMAGE="{{asset('storage/media/')}}";
-    </script>
 
-  </head>
-  <body class="productPage"> 
-   <!-- wpf loader Two -->
-    <div id="wpf-loader-two">          
-      <div class="wpf-loader-two-inner">
-        <span>Loading</span>
-      </div>
-    </div> 
-    <!-- / wpf loader Two -->       
+  <script>
+    var PRODUCT_IMAGE = "{{asset('storage/media/')}}";
+  </script>
+
+</head>
+
+<body class="productPage">
+  <!-- wpf loader Two -->
+  <div id="wpf-loader-two">
+    <div class="wpf-loader-two-inner">
+      <span>Loading</span>
+    </div>
+  </div>
+  <!-- / wpf loader Two -->
   <!-- SCROLL TOP BUTTON -->
-    <a class="scrollToTop" href="#"><i class="fa fa-chevron-up"></i></a>
+  <a class="scrollToTop" href="#"><i class="fa fa-chevron-up"></i></a>
   <!-- END SCROLL TOP BUTTON -->
 
 
@@ -55,7 +57,7 @@
             <div class="aa-header-top-area">
               <!-- start header top left -->
               <div class="aa-header-top-left">
-                
+
                 <!-- start cellphone -->
                 <div class="cellphone hidden-xs">
                   <p><span class="fa fa-phone"></span>00-62-658-658</p>
@@ -70,9 +72,9 @@
                   <li><a href="{{url('/order')}}">My Order</a></li>
                   <li><a href="{{url('/logout')}}">Logout</a></li>
                   @else
-                    <li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
+                  <li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
                   @endif
-                  
+
 
                 </ul>
               </div>
@@ -100,12 +102,12 @@
                 <!-- <a href="javascript:void(0)"><img src="img/logo.jpg" alt="logo img"></a> -->
               </div>
               <!-- / logo  -->
-               <!-- cart box -->
+              <!-- cart box -->
               @php
               $getAddToCartTotalItem=getAddToCartTotalItem();
               $totalCartItem=count($getAddToCartTotalItem);
               $totalPrice=0;
-              @endphp 
+              @endphp
               <div class="aa-cartbox">
                 <a class="aa-cart-link" href="#" id="cartBox">
                   <span class="fa fa-shopping-basket"></span>
@@ -113,8 +115,8 @@
                   <span class="aa-cart-notify">{{$totalCartItem}}</span>
                 </a>
                 <div class="aa-cartbox-summary">
-               @if($totalCartItem>0)
-                
+                  @if($totalCartItem>0)
+
                   <ul>
                     @foreach($getAddToCartTotalItem as $cartItem)
 
@@ -128,7 +130,7 @@
                         <p>{{$cartItem->qty}} * BDT {{$cartItem->price}}</p>
                       </div>
                     </li>
-                    @endforeach                  
+                    @endforeach
                     <li>
                       <span class="aa-cartbox-total-title">
                         Total
@@ -139,8 +141,8 @@
                     </li>
                   </ul>
                   <a class="aa-cartbox-checkout aa-primary-btn" href="{{url('/cart')}}">Cart</a>
-               
-                @endif
+
+                  @endif
                 </div>
               </div>
               <!-- / cart box -->
@@ -151,7 +153,7 @@
                   <button type="button" onclick="funSearch()"><span class="fa fa-search"></span></button>
                 </form>
               </div>
-              <!-- / search box -->             
+              <!-- / search box -->
             </div>
           </div>
         </div>
@@ -172,86 +174,88 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
-            </button>          
+            </button>
           </div>
-          
+
           <div class="navbar-collapse collapse">
             <!-- Left nav -->
             {!! getTopNavCat() !!}
-          </div><!--/.nav-collapse -->
+          </div>
+          <!--/.nav-collapse -->
         </div>
-      </div>       
+      </div>
     </div>
   </section>
   <!-- / menu -->
   <!-- Start slider -->
-  
+
   @section('container')
-  @show      
-  
-  <!-- footer -->  
+  @show
+
+  <!-- footer -->
   <footer id="aa-footer">
     <!-- footer bottom -->
     <div class="aa-footer-top">
-     <div class="container">
+      <div class="container">
         <div class="row">
-        <div class="col-md-12">
-          <div class="aa-footer-top-area">
-            <div class="row">
-              <div class="col-md-3 col-sm-6">
-                <div class="aa-footer-widget">
-                  <h3>Main Menu</h3>
-                  <ul class="aa-footer-nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Our Services</a></li>
-                    <li><a href="#">Our Products</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-md-3 col-sm-6">
-                <div class="aa-footer-widget">
+          <div class="col-md-12">
+            <div class="aa-footer-top-area">
+              <div class="row">
+                <div class="col-md-3 col-sm-6">
                   <div class="aa-footer-widget">
-                    <h3>Knowledge Base</h3>
+                    <h3>Main Menu</h3>
                     <ul class="aa-footer-nav">
-                      <li><a href="#">Delivery</a></li>
-                      <li><a href="#">Returns</a></li>
-                      <li><a href="#">Services</a></li>
-                      <li><a href="#">Discount</a></li>
-                      <li><a href="#">Special Offer</a></li>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">Our Services</a></li>
+                      <li><a href="#">Our Products</a></li>
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Contact Us</a></li>
                     </ul>
                   </div>
                 </div>
-              </div>
-              <div class="col-md-3 col-sm-6">
-                <div class="aa-footer-widget">
+                <div class="col-md-3 col-sm-6">
                   <div class="aa-footer-widget">
-                    <h3>Useful Links</h3>
-                    <ul class="aa-footer-nav">
-                      <li><a href="#">Site Map</a></li>
-                      <li><a href="#">Search</a></li>
-                      <li><a href="#">Advanced Search</a></li>
-                      <li><a href="#">Suppliers</a></li>
-                      <li><a href="#">FAQ</a></li>
-                    </ul>
+                    <div class="aa-footer-widget">
+                      <h3>Knowledge Base</h3>
+                      <ul class="aa-footer-nav">
+                        <li><a href="#">Delivery</a></li>
+                        <li><a href="#">Returns</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Discount</a></li>
+                        <li><a href="#">Special Offer</a></li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-md-3 col-sm-6">
-                <div class="aa-footer-widget">
+                <div class="col-md-3 col-sm-6">
                   <div class="aa-footer-widget">
-                    <h3>Contact Us</h3>
-                    <address>
-                      <p> 25 Astor Pl, NY 10003, USA</p>
-                      <p><span class="fa fa-phone"></span>+1 212-982-4589</p>
-                      <p><span class="fa fa-envelope"></span>dailyshop@gmail.com</p>
-                    </address>
-                    <div class="aa-footer-social">
-                      <a href="#"><span class="fa fa-facebook"></span></a>
-                      <a href="#"><span class="fa fa-twitter"></span></a>
-                      <a href="#"><span class="fa fa-google-plus"></span></a>
-                      <a href="#"><span class="fa fa-youtube"></span></a>
+                    <div class="aa-footer-widget">
+                      <h3>Useful Links</h3>
+                      <ul class="aa-footer-nav">
+                        <li><a href="#">Site Map</a></li>
+                        <li><a href="#">Search</a></li>
+                        <li><a href="#">Advanced Search</a></li>
+                        <li><a href="#">Suppliers</a></li>
+                        <li><a href="#">FAQ</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                  <div class="aa-footer-widget">
+                    <div class="aa-footer-widget">
+                      <h3>Contact Us</h3>
+                      <address>
+                        <p> 25 Astor Pl, NY 10003, USA</p>
+                        <p><span class="fa fa-phone"></span>+1 212-982-4589</p>
+                        <p><span class="fa fa-envelope"></span>dailyshop@gmail.com</p>
+                      </address>
+                      <div class="aa-footer-social">
+                        <a href="#"><span class="fa fa-facebook"></span></a>
+                        <a href="#"><span class="fa fa-twitter"></span></a>
+                        <a href="#"><span class="fa fa-google-plus"></span></a>
+                        <a href="#"><span class="fa fa-youtube"></span></a>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -260,46 +264,45 @@
           </div>
         </div>
       </div>
-     </div>
     </div>
     <!-- footer-bottom -->
     <div class="aa-footer-bottom">
       <div class="container">
         <div class="row">
-        <div class="col-md-12">
-          <div class="aa-footer-bottom-area">
-            <p>Designed by <a href="http://www.markups.io/">MarkUps.io</a></p>
-            <div class="aa-footer-payment">
-              <span class="fa fa-cc-mastercard"></span>
-              <span class="fa fa-cc-visa"></span>
-              <span class="fa fa-paypal"></span>
-              <span class="fa fa-cc-discover"></span>
+          <div class="col-md-12">
+            <div class="aa-footer-bottom-area">
+              <p>Designed by <a href="http://www.markups.io/">MarkUps.io</a></p>
+              <div class="aa-footer-payment">
+                <span class="fa fa-cc-mastercard"></span>
+                <span class="fa fa-cc-visa"></span>
+                <span class="fa fa-paypal"></span>
+                <span class="fa fa-cc-discover"></span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   </footer>
   <!-- / footer -->
   @php
   if(isset($_COOKIE['login_email']) && isset($_COOKIE['login_pwd'])){
-    $login_email=$_COOKIE['login_email'];
-    $login_pwd=$_COOKIE['login_pwd'];
-    $is_remember="checked='checked'";
+  $login_email=$_COOKIE['login_email'];
+  $login_pwd=$_COOKIE['login_pwd'];
+  $is_remember="checked='checked'";
   } else{
-    $login_email='';
-    $login_pwd='';
-    $is_remember="";
-  }   
+  $login_email='';
+  $login_pwd='';
+  $is_remember="";
+  }
 
-  @endphp    
-  <!-- Login Modal -->  
+  @endphp
+  <!-- Login Modal -->
   <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">                      
+      <div class="modal-content">
         <div class="modal-body">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           <div id="popup_login">
             <h4>Login or Register</h4>
             <form class="aa-login-form" id="frmLogin">
@@ -308,12 +311,20 @@
               <label for="">Password<span>*</span></label>
               <input type="password" placeholder="Password" name="str_login_password" required value="{{$login_pwd}}">
               <button class="aa-browse-btn" type="submit" id="btnLogin">Login</button>
+
+              <div class="flex items-center justify-end mt-4">
+                <a href="{{ url('auth/google') }}">
+                  <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+                </a>
+              </div>
+
+              
               <label for="rememberme" class="rememberme"><input type="checkbox" id="rememberme" name="rememberme" {{$is_remember}}> Remember me </label>
 
               <div id="login_msg"></div>
 
               <p class="aa-lost-password"><a href="javascript:void(0)" onclick="forgot_password()">Lost your password?</a></p>
-              
+
               <div class="aa-register-now">
                 Don't have an account?<a href="{{url('registration')}}">Register now!</a>
               </div>
@@ -328,7 +339,7 @@
               <button class="aa-browse-btn" type="submit" id="btnForgot">Submit</button>
               <br><br>
               <div id="forgot_msg"></div>
-             
+
               <div class="aa-register-now">
                 Login Form?<a href="javascript:void(0)" onclick="show_login_popup()">Login now!</a>
               </div>
@@ -336,22 +347,23 @@
             </form>
           </div>
 
-        </div>                        
+        </div>
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
-  </div>    
+  </div>
 
   <!-- jQuery library -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="{{asset('front_assets/js/bootstrap.js')}}"></script>  
+  <script src="{{asset('front_assets/js/bootstrap.js')}}"></script>
   <script type="text/javascript" src="{{asset('front_assets/js/jquery.smartmenus.js')}}"></script>
-  <script type="text/javascript" src="{{asset('front_assets/js/jquery.smartmenus.bootstrap.js')}}"></script>  
+  <script type="text/javascript" src="{{asset('front_assets/js/jquery.smartmenus.bootstrap.js')}}"></script>
   <script src="{{asset('front_assets/js/sequence.js')}}"></script>
-  <script src="{{asset('front_assets/js/sequence-theme.modern-slide-in.js')}}"></script>  
+  <script src="{{asset('front_assets/js/sequence-theme.modern-slide-in.js')}}"></script>
   <script type="text/javascript" src="{{asset('front_assets/js/jquery.simpleGallery.js')}}"></script>
   <script type="text/javascript" src="{{asset('front_assets/js/jquery.simpleLens.js')}}"></script>
   <script type="text/javascript" src="{{asset('front_assets/js/slick.js')}}"></script>
   <script type="text/javascript" src="{{asset('front_assets/js/nouislider.js')}}"></script>
-  <script src="{{asset('front_assets/js/custom.js')}}"></script> 
-  </body>
+  <script src="{{asset('front_assets/js/custom.js')}}"></script>
+</body>
+
 </html>
